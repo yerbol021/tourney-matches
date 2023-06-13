80cafe0107237be1dcea05141e64b1fa484040fa
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-13 15:50:58 -0700

    Extract one match object

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -2,11 +2,14 @@ import React from "react"; //optional
 import matchData from "../data/matchData";
 import Match from "./Match";
 
-function MatchList() {
+function MatchList(props) {
+
+  const oneMatch = matchData[0];
+
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      <Match />
+      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
     </section>
   );
 }
