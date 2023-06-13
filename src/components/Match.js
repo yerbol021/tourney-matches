0794befcbce0b5384572e079041d1f0f443e9759
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-13 16:29:17 -0700

    Conditional Rendering

--- src/components/Match.js
+++ src/components/Match.js
@@ -4,12 +4,11 @@ function Match(props) {
   return (
     <article className="Match">
       <h1>
-      {props.players[0]} <span>vs</span> {props.players[1]}
+        {props.players[0]} <span>vs</span> {props.players[1]}
       </h1>
-      {/* To be shown when there is a winner */}
-      <h2>{props.winner} is the winner by {props.scoreDifference}!</h2>
-      {/* To be shown when there is no winner */}
-      <h2>No winners yet!</h2>
+      {props.winner && <h2>{props.winner} is the winner by {props.scoreDifference}!</h2>}
+
+      {!props.winner && <h2>No winners yet!</h2>}
     </article>
   );
 };
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,15 @@
 import React from "react"; //optional
 import playerData from "../data/playerData";
 
-function Player(props) {
-
+function Player (props) {
   return (
     <article className="Player">
       <h1>{props.firstName} <span>{props.gamerTag}</span> {props.lastName}</h1>
-      {/* To be shown when there is no wins */}<h2 className="zero">Currently with no wins </h2>:(
-      {/* To be shown when there is 1 win */}<h2>Currently at 1 win</h2>
-      {/* To be shown when there is more than one win */}<h2>Currently at {props.wins} wins</h2>)
+      {props.wins === 0 && <h2 className="zero">Currently with no wins</h2>}
+      {props.wins === 1 && <h2>Currently at {props.wins} win</h2>}
+      {props.wins > 1 && <h2>Currently at {props.wins} wins</h2>}
     </article>
   );
-}
+};
 
 export default Player;
