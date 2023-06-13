38353ceeca619fc34370795f963b139aeb60bda8
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-13 15:02:01 -0700

    added funcs to playerlist

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -4,6 +4,8 @@ import playerData from '../data/playerData';
 import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
 function PlayerList() {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
